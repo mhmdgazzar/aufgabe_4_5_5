28c2718cdd7156bee0e97e98aa8843f662b08882
mhmdgazzar <mhmdgazzar@gmail.com>
2024-04-22 16:53:31 +0200

    screens

--- aufgabe_455/lib/home_screen.dart
+++ aufgabe_455/lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aufgabe_455/product_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Home Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ProductScreen()));
+        },
+        child: const Icon(Icons.shopping_cart),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ProductScreen()));
+                  },
+                  child: const Text('See product page')),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ aufgabe_455/lib/product_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatelessWidget {
+  const ProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('Product Screen'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+                width: 300,
+                'https://vangogh.teespring.com/v3/image/tyaCtcSxdGFsGiM3gBw1WcAvr1k/800/800.jpg'),
+            const Text('Flutter App', style: TextStyle(fontSize: 30)),
+            const SizedBox(height: 50),
+            const Text('Flutter app development'),
+            const Text('400€')
+          ],
+        ),
+      ),
+    );
+  }
+}
